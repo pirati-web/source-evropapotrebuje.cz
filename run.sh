28e58a739d671b6523c73d0bc1d7df5f72693670
Jitka Novotná <jitka@ucw.cz>
2019-05-15 17:05:40 +0200

    initial commit

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "x$1" == "x+" ]]; then
-  ./manage.py runserver_plus 127.0.0.1:8011
+  python3 ./manage.py runserver_plus 127.0.0.1:8011
 else
-  ./manage.py runserver --insecure 127.0.0.1:8011
+  python3 ./manage.py runserver --insecure 127.0.0.1:8011
 fi
